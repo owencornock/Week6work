35be3eea95fa5135ca53b652d5a76570a49f644d
owencornock <owen52new@gmail.com>
2022-04-05 10:58:34 +0100

    more js work

---
+++ Week 6/javascript/script.js
@@ -0,0 +1,33 @@
+// Use strict and console
+
+/* 
+    use strict:
+    forces us to write secure javascript
+    stops us using protected words, making global variables etc.
+*/
+
+    // `use strict`
+
+let myVar =1;
+
+
+console.log(`Hello World`);
+
+console.info(`this is info`);
+
+console.warn(`this is a warning`);
+
+console.error(`This is a error`);
+
+console.log(`%c` + `some text`, `color:black; background-color:white; font-size: 30px; padding: 15px;`);
+
+console.log(`First Name`);
+
+console.warn();(`Second Name`);
+
+console.info(`Where are you from`);
+
+console.error(`And your star sign`);
+
+
+console.log(`%c` + `My Message`, `font-style:bold; color:orange; font-family:fantasy; background-color:black; padding:10px;`);
