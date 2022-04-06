7a859061f1fa43a40c69b7e3d648411456c2c422
owencornock <owen52new@gmail.com>
2022-04-06 16:31:39 +0100

    fresh pus, for a fresh day

---
+++ Week 6/day3/dom.js
@@ -0,0 +1,13 @@
+export const textInput = document.getElementById(`textInput`);
+
+export const textButon = document.getElementById(`textButton`);
+
+export const textOutput = document.getElementById(`textOutput`);
+
+// export const newHead = document.getElementById(`newHead`);
+
+// export const newText = document.getElementById(`newText`);
+
+// export const btn = document.getElementById(`btn`);
+
+// export const huge = document.getElementById(`huge`);
--- Week 6/day3/script.js
+++ Week 6/day3/script.js
@@ -1,18 +1,99 @@
-`use strict`
+'use strict'
 
-for ( let a =1; a<=100; a++) {
-    if (a % 5 == 0 && a % 3 == 0 ) {
-        console.log(`FizzBuzz`);
-    }
-    else if (a % 3 == 0) {
-        console.log(`Fizz`);
-    }
-    else if (a % 5 == 0) {
-        console.log(`Buzz`);
-    }
-    else {
-        console.log(a);
-    }
+// for ( let a =1; a<=100; a++) {
+//     if (a % 5 == 0 && a % 3 == 0 ) {
+//         console.log(`FizzBuzz`);
+//     }
+//     else if (a % 3 == 0) {
+//         console.log(`Fizz`);
+//     }
+//     else if (a % 5 == 0) {
+//         console.log(`Buzz`);
+//     }
+//     else {
+//         console.log(a);
+//     }
+// }
+
+//SCOPE
+
+/*
+    there are two types of scope
+    Local and Global
+*/
+
+// function test() {
+//     let x = 'foo'
+//     if (x == 'foo') {
+//         let y = 'boo'
+//     }
+//     console.log(x);
+
+//     console.log(y);
+// }
+
+// test();
+
+// function doSomething() {
+//     console.log(a);
+//     console.log(foo());
+//     let a = 1;
+//     function foo() {
+//       return 2;
+//     }
+//   }
+//   doSomething();
+
+//DOM 
+
+//document object model
+
+/* Page
+    HTML
+        head
+            metadata
+        body
+            headers
+            paras
+            divs
+*/
+
+import * as DOM from './dom.js';
+
+let str = "hello";
+
+// DOM.textButon.onclick = () => DOM.textOutput.innerHTML = `${str}` ;
+
+let addToList = str => {
+    let child = document.createElement(`li`);
+    child.innerHTML = str;
+    DOM.textOutput.appendChild(child);
 }
 
-puts (a%3 == 0) ? ((a%5 == 0) ? "FizzBuzz" : "Buzz") : ((a%5 == 0) ? "Fizz" : a)
+// DOM.textButon.onclick = () => DOM.textOutput.innerHTML = DOM.textInput.value;
+
+// DOM.textButon.onclick = () => addToList(DOM.textInput.value);
+
+// let newHead = document.createElement(`h1`);
+// let newText = document.createTextNode("Big Boy Head");
+
+// DOM.newHead.appendChild(newText);
+// DOM.document.body.appendChild(newHead);
+
+// let btn = document.querySelector('button');
+
+// DOM.btn.onclick = () => addEventListener('click',foo);
+
+let ele = document.querySelector("h1");
+ele.style.color = "red";
+
+let paras = document.querySelectorAll('p');
+for(let p of paras){
+    p.style.colour = 'blue';
+}
+
+let em = document.createElement('em');
+let strong = document.querySelector('strong');
+let div = document.querySelector('div');
+em.textContent = "Replaced!"; 
+div.replaceChild(em,strong);
